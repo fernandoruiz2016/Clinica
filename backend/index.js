999cb22fad2aa1f4b23a41ce9e14cbb5f2a9096a
Fernando Ruiz <fernandoruiz696@gmail.com>
2026-02-26 22:22:32 -0500

    dashboard

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const crearTablas = require("./database/crearTablas");
 const pacientesApi = require("./apis/pacientes");
 const medicosApi = require("./apis/medicos");
 const citasApi = require("./apis/citas");
+const dashboardRoutes = require("./apis/dashboard");
+
 
 const app = express();
 
@@ -16,6 +18,7 @@ app.use(express.json());
 app.use("/pacientes", pacientesApi);
 app.use("/medicos", medicosApi);
 app.use("/citas", citasApi);
+app.use("/dashboard", dashboardRoutes);
 
 const port = process.env.SERVER_PORT;
 
