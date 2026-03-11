fdc65b268014ad3ebb43ddd049b24d81f7d03971
Fernando Ruiz <fernandoruiz696@gmail.com>
2026-03-10 20:32:30 -0500

    Bug fixed: enviaba a login al recarga la pagina

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -29,5 +29,5 @@ export const routes: Routes = [
     ],
   },
 
-  // { path: '**', redirectTo: 'login' },
+  { path: '**', redirectTo: 'login' },
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('ProyVet');
+export class App implements OnInit {
+  isReady = false;
+  private platformId = inject(PLATFORM_ID);
+  private router = inject(Router);
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem('token');
+      const isLoginPage = window.location.pathname.includes('/login');
+
+      if (!token && !isLoginPage) {
+        // No hay sesión: Al login
+        this.router.navigate(['/login']);
+      } else if (token && isLoginPage) {
+        // Ya está logueado: No lo dejes entrar al login, mándalo al dashboard
+        this.router.navigate(['/']);
+      }
+    }
+  }
 }
