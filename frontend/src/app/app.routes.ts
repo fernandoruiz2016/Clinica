2075ce10dec0dce98b67185f367fe1093225e198
Fernando Ruiz <fernandoruiz696@gmail.com>
2026-03-20 21:32:27 -0500

    fix bugs

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,9 @@ import { authGuard } from './shared/guards/auth-guard';
 import { Login } from './features/login/pages/login/login';
 import { UsuariosComponent } from './features/usuarios/pages/usuarios-component/usuarios';
 import { CrearUsuario } from './features/usuarios/pages/crear-usuario/crear-usuario';
+import { PacientesComponent } from './features/pacientes/pages/pacientes-component/pacientes-component';
+import { MedicosComponent } from './features/medicos/pages/medicos-component/medicos-component';
+import { ReportesComponent } from './features/reportes/pages/reportes-component/reportes-component';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
@@ -18,12 +21,12 @@ export const routes: Routes = [
     canActivate: [authGuard],
     children: [
       { path: '', component: Home },
-      // { path: 'pacientes', component: PacientesComponent },
+      { path: 'pacientes', component: PacientesComponent },
       { path: 'citas', component: CitasComponent },
       { path: 'citas/crear', component: CrearCita },
       { path: 'citas/editar/:id', component: EditarCita },
-      // { path: 'medicos', component: MedicosComponent },
-      // { path: 'reportes', component: ReportesComponent },
+      { path: 'medicos', component: MedicosComponent },
+      { path: 'reportes', component: ReportesComponent },
       { path: 'usuarios', component: UsuariosComponent },
       { path: 'usuarios/crear', component: CrearUsuario },
     ],
---
+++ frontend/src/app/features/pacientes/pages/pacientes-component/pacientes-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pacientes-component',
+  imports: [],
+  templateUrl: './pacientes-component.html',
+  styleUrl: './pacientes-component.css',
+})
+export class PacientesComponent {
+
+}
